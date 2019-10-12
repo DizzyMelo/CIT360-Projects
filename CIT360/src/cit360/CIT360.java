ab5fc57811d457c2d2aff00d448ae44035499bc8
DizzyMelo <daniel.melo42@outlook.com>
2019-10-12 07:22:27 -0300

    collections, threads, mvc, hibernate

---
+++ CIT360/src/cit360/CIT360.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit360;
+
+/**
+ *
+ * @author kss_s
+ */
+public class CIT360 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome = "Daniel";
+        System.out.println("hello world " + nome);
+    }
+    
+}
